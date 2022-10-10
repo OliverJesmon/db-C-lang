e7d766c45aba7cfc3e69b53d80ff31b1b43255f4
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2022-10-10 13:09:09 +0530

    Create database.c

---
+++ database.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct index
+{
+    char alias[20];
+    int age;
+    char gen;
+};
+int main()
+{
+    struct index in[50];
+    int i,n,j;
+    int choice;
+    printf("Created by Oliver Jesmon.");
+    printf("\n\nEnter no. of people: ");
+    scanf("%d",&n);
+    system("clear");
+    for(i=0; i<n; i++)
+    {
+        printf("\nEnter name:");
+        scanf("%s",in[i].alias);
+        printf("\nEnter age:");
+        scanf("%d",&in[i].age);
+        printf("\nGender:1.Male\t2.Female\n");
+        scanf("%d",&choice);
+        system("clear");
+        switch(choice)
+        {
+        case 1:
+            in[i].gen='M';
+            break;
+        case 2:
+            in[i].gen='F';
+            break;
+        default:
+            exit(0);
+            break;
+        }
+    }
+    system("clear");
+    printf("  Name\t\t\tAge   Gender");
+    for(i=0; i<n; i++)
+    {
+        printf("\n%d.%s\t",i+1,in[i].alias);
+        printf("\t");
+        printf("%d",in[i].age);
+        printf("    %c",in[i].gen);
+    }
+    return 0;//Signing out Jesmon
+}
